a55c83bb8fc32292008bd3c6fab1f1476b93b8fa
Andreas Adam <andreas.adam@composableweb.com>
2023-03-17 14:39:14 +0100

    fix: typo

--- apps/demo/src/pages/index.tsx
+++ apps/demo/src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Compoenent Library Demo</title>
+        <title>Component Library Demo</title>
       </Head>
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
